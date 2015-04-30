8072aaa63192c03cf0994da4fd2afcfdf85b2130
Junsung Lim <wns349@kaist.ac.kr>
2015-04-30 20:19:09 +0900

    simple layout for bluetooth demo

--- src/cj/js/hak_yo/MainActivity.java
+++ src/cj/js/hak_yo/MainActivity.java
@@ -2,8 +2,6 @@ package cj.js.hak_yo;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +9,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 	}
+
 }
