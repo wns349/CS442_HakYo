b6e9ffd835f065bc244a193b57b961e0fcbc209a
Junsung Lim <wns349@kaist.ac.kr>
2015-05-01 16:12:38 +0900

    added scanning functionality  - shows in listview

---
+++ src/cj/js/hak_yo/DeviceListAdapter.java
@@ -0,0 +1,85 @@
+package cj.js.hak_yo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.altbeacon.beacon.Beacon;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class DeviceListAdapter extends BaseAdapter {
+
+	private final Context context;
+	private final List<Beacon> beacons;
+
+	public DeviceListAdapter(Context context) {
+		this.context = context;
+		this.beacons = new ArrayList<Beacon>();
+	}
+
+	@Override
+	public int getCount() {
+		return beacons.size();
+	}
+
+	@Override
+	public Beacon getItem(int position) {
+		return beacons.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater li = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = li.inflate(R.layout.device_listitem, null);
+
+		Beacon beacon = this.beacons.get(position);
+		if (beacon == null) {
+			return null;
+		}
+
+		TextView txtName = (TextView) view.findViewById(R.id.device_item_name);
+		TextView txtAddress = (TextView) view
+				.findViewById(R.id.device_item_address);
+		TextView txtDistance = (TextView) view
+				.findViewById(R.id.device_item_distance);
+		TextView txtRssi = (TextView) view.findViewById(R.id.device_item_rssi);
+
+		txtName.setText(beacon.getBluetoothName());
+		txtAddress.setText(beacon.getBluetoothAddress());
+		txtDistance.setText(String.valueOf(beacon.getDistance()));
+		txtRssi.setText(String.valueOf(beacon.getRssi()));
+
+		return view;
+	}
+
+	public void addBeacon(Beacon beacon) {
+		int i = 0;
+		boolean exist = false;
+		for (i = 0; i < this.beacons.size(); i++) {
+			if (this.beacons.get(i).getBluetoothAddress()
+					.equals(beacon.getBluetoothAddress())) {
+				exist = true;
+				break;
+			}
+		}
+
+		if (!exist) {
+			this.beacons.add(beacon);
+		} else {
+			this.beacons.remove(i);
+			this.beacons.add(i, beacon);
+		}
+	}
+}
--- src/cj/js/hak_yo/MainActivity.java
+++ src/cj/js/hak_yo/MainActivity.java
@@ -1,15 +1,133 @@
 package cj.js.hak_yo;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.Collection;
+import java.util.Iterator;
 
-public class MainActivity extends Activity {
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements BeaconConsumer {
+	private static final String TAG = "CJS";
+
+	private static final int REQUEST_ENABLE_BT = 1928;
+
+	private BluetoothAdapter btAdapter = null;
+
+	private BeaconManager beaconManager = null;
+
+	private DeviceListAdapter deviceListAdapter = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		initializeViews();
+
+		initializeBluetooth();
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQUEST_ENABLE_BT:
+			if (resultCode == Activity.RESULT_CANCELED) {
+				Toast.makeText(this, "Bluetooth must be enabled.",
+						Toast.LENGTH_LONG).show();
+				finish();
+			} else if (resultCode == Activity.RESULT_OK) {
+				scanBluetoothDevices();
+			}
+			break;
+		default:
+			break;
+		}
+	};
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (beaconManager != null) {
+			beaconManager.unbind(this);
+		}
+	}
+
+	private void initializeViews() {
+		ListView listDevices = (ListView) findViewById(R.id.list_devices);
+		deviceListAdapter = new DeviceListAdapter(this);
+
+		listDevices.setAdapter(deviceListAdapter);
+		deviceListAdapter.notifyDataSetChanged();
+	}
+
+	private void initializeBluetooth() {
+		btAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (btAdapter == null) {
+			// Bluetooth not supported by the device
+			Toast.makeText(this, "Bluetooth not found.", Toast.LENGTH_LONG)
+					.show();
+			finish();
+		}
+
+		if (!btAdapter.isEnabled()) {
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		} else {
+			scanBluetoothDevices();
+		}
+	}
+
+	private void scanBluetoothDevices() {
+		beaconManager = BeaconManager.getInstanceForApplication(this);
+		beaconManager.bind(this);
+	}
+
+	@Override
+	public void onBeaconServiceConnect() {
+		beaconManager.setRangeNotifier(new RangeNotifier() {
+			@Override
+			public void didRangeBeaconsInRegion(Collection<Beacon> beacons,
+					Region region) {
+				if (beacons.size() > 0) {
+					Iterator<Beacon> beaconItr = beacons.iterator();
+					while (beaconItr.hasNext()) {
+						final Beacon beacon = beaconItr.next();
+						Log.d(TAG,
+								"Beacon found: " + beacon.getBluetoothName()
+										+ " / " + beacon.getBluetoothAddress()
+										+ " / " + beacon.getDistance() + " / "
+										+ beacon.getRssi());
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								deviceListAdapter.addBeacon(beacon);
+								deviceListAdapter.notifyDataSetChanged();
+							}
+						});
+					}
+				}
+			}
+		});
+
+		try {
+			Region region = new Region("myRangingUniqueId", null, null, null);
+			beaconManager.startRangingBeaconsInRegion(region);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
